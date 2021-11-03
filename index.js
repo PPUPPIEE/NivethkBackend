022a01b10d79cd6ebac35fa32d53ecaead596697
yoyozapp <peepeebs56@gmail.com>
2021-11-03 19:35:55 +0700

    add cor again

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 var port = process.env.PORT || 5000;
 app.use(bodyParser.urlencoded({
     extended: true
@@ -19,11 +19,7 @@ mongoose.connect("mongodb+srv://NewXI:NewXI@cluster0.xthd5.mongodb.net/where")
 .then(()=>console.log("DB connect Successful"))
 .catch((err)=>{console.log(err)});
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
- next();
-});
+app.use(cors());
 
 app.use("/api/province",getProvince)
 app.use("/api/addlocation",postAddlocation)
