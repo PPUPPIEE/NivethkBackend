acbd9888fb84b6071b3f475d87f578e58bcb6282
Aniwatchuychay <Aniwat.Chuychay@gmail.com>
2021-11-10 20:13:50 +0700

    Update api delete

--- route/deletelocation.js
+++ route/deletelocation.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Status = require("../model/updateStatus")
 const router = require("express").Router();
 
-router.delete("/", (req, res, next) => {
-    Status.remove({ name: req.body.name })
+router.delete("/:Name", (req, res, next) => {
+    const Name = req.params.Name;
+    Status.remove({ name: Name })
         .exec()
         .then(data => {
-            res.json({message:"ทำการลบ " + req.body.name + " เสร็จสิ้น"})
+            res.json({message:"ทำการลบ " + req.params.Name + " เสร็จสิ้น"})
         })
         .catch(err => {
             res.json({ message: err })
